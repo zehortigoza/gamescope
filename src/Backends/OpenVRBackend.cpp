b708345fc91a6b7078071ea01b5eed4692434b58
Joshua Ashton <joshua@froggi.es>
2024-09-11 04:21:07 +0100

    OpenVRBackend: Fix OverlayClosed when not in steam mode

--- src/Backends/OpenVRBackend.cpp
+++ src/Backends/OpenVRBackend.cpp
@@ -908,7 +908,7 @@ namespace gamescope
                                     case vr::VREvent_OverlayClosed:
                                     case vr::VREvent_Quit:
                                     {
-                                        if ( bIsSteam )
+                                        if ( !steamMode || bIsSteam )
                                         {
                                             if ( !plane.IsSubview() )
                                             {
